d5e8814c7fc6fa2529ba43c0cbde1b3d5173e0ff
Pythonology <pythonology@gmail.com>
2020-11-22 20:32:33 -0500

    Add `BeatTogether.Core.Hosting` project and an extension method for binding configuration

---
+++ BeatTogether.Core.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeatTogether.Core.Hosting.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConfiguration<T>(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            string sectionKey) where T : class =>
+            services.AddSingleton(configuration.GetSection(sectionKey).Get<T>());
+    }
+}
