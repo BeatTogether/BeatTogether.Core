87e2649fa17a8d545c39a43c67f10b7e707b6dd5
Pythonology <pythonology@gmail.com>
2021-01-01 21:40:50 -0500

    Add RabbitMQ configuration

---
+++ BeatTogether.Core.Messaging/Configuration/RabbitMQConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace BeatTogether.Core.Messaging.Configuration
+{
+    public class RabbitMQConfiguration
+    {
+        public string EndPoint { get; set; } = "tcp://127.0.0.1:5672";
+    }
+}
