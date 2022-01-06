bf32efb49a5197882713e11f813326ef814fe0cc
MT <littlehappytroll@gmail.com>
2022-01-06 00:47:25 -0500

    Bump message protocol version

--- BeatTogether.Core.Messaging/Implementations/MessageReader.cs
+++ BeatTogether.Core.Messaging/Implementations/MessageReader.cs
@@ -9,8 +9,8 @@ namespace BeatTogether.Core.Messaging.Implementations
 {
     public class MessageReader : IMessageReader
     {
-        protected virtual uint MinimumProtocolVersion => 5;
-        protected virtual uint MaximumProtocolVersion => 5;
+        protected virtual uint MinimumProtocolVersion => 6;
+        protected virtual uint MaximumProtocolVersion => 6;
 
         private readonly Dictionary<uint, IMessageRegistry> _messageRegistries;
 
--- BeatTogether.Core.Messaging/Implementations/MessageWriter.cs
+++ BeatTogether.Core.Messaging/Implementations/MessageWriter.cs
@@ -9,7 +9,7 @@ namespace BeatTogether.Core.Messaging.Implementations
 {
     public class MessageWriter : IMessageWriter
     {
-        protected virtual uint ProtocolVersion => 5;
+        protected virtual uint ProtocolVersion => 6;
 
         private readonly Dictionary<uint, IMessageRegistry> _messageRegistries;
 
