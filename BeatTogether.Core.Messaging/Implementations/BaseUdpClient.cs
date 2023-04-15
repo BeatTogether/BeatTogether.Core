3282186f325fd29856a1b60377eac10ffcd8a29d
cubic <cubicgraphics98@gmail.com>
2023-04-15 23:23:31 +0100

    Bump ver

--- BeatTogether.Core.Messaging/Implementations/BaseUdpClient.cs
+++ BeatTogether.Core.Messaging/Implementations/BaseUdpClient.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Net;
-using System.Net.Sockets;
-using System.Threading;
 using System.Threading.Tasks;
 using AsyncUdp;
 using BeatTogether.Core.Messaging.Abstractions;
